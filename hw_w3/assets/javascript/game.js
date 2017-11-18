e91d5b63dbc7b2e8f4e48d468e3f83ac17bd11af
Kevin Castro <kcastro.build@gmail.com>
2017-11-17 16:41:24 -0800

    more structure changes to index, setting up js

---
+++ hw_w3/assets/javascript/game.js
@@ -0,0 +1,19 @@
+//below will be the code for the game to be run -- note the document.ready function, allowing me to link in the head
+
+$(document).ready(function() {
+	//make an array of the words to be used in game
+	// while testing, will leave placeholders -- add in later
+	var game = {
+		var wordList = [
+			"word1",
+			"word2",
+			"word3",
+		],
+
+
+	}
+
+
+
+
+});
