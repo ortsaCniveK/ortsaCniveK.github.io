3ff6e87d34d9e604cd6fa96d10ffcc8f9228e1d9
Kevin Castro <kcastro.build@gmail.com>
2018-02-03 22:28:43 -0800

    create successful test, received correct output

--- hw_w12/bamazonCustomer.js
+++ hw_w12/bamazonCustomer.js
@@ -3,13 +3,19 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
-	user: root,
-	password: '',
+	user: 'root',
+	password: 'root',
 	database: 'bamazon'
 });
 
 connection.connect((err) => {
 	if (err) throw err;
-	console.log(`connected as id {$connection.threadId}`);
-	// start();
+	console.log('connected as id' + connection.threadId);
 });
+
+connection.query(
+	'select * from products',
+	(err, res) => {
+		if (err) throw err;
+		console.log(res);
+	});
