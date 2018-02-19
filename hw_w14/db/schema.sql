d6b200af1c3def71572bf78806a29576b135d1b2
Kevin Castro <kcastro.build@gmail.com>
2018-02-19 11:01:47 -0800

    establish schema and seeds for local server

---
+++ hw_w14/db/schema.sql
@@ -0,0 +1,8 @@
+create database burgers_db;
+use burgers_db;
+
+create table burgers(
+    id int not null auto_increment primary key,
+    burger_name varchar(100),
+    devoured boolean default 0
+);
