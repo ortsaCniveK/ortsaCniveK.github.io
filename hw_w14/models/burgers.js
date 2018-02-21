c581dbc89e981bb5fa7995ca46435cd8700c5e6a
Kevin Castro <kcastro.build@gmail.com>
2018-02-21 07:18:08 -0800

    change return values for insert and update funcs

--- hw_w14/models/burgers.js
+++ hw_w14/models/burgers.js
@@ -10,12 +10,15 @@ const burgers = {
     },
     insertBurger : (name) => {
         orm.insertOne('burgers', 'burger_name', name, (result) => {
-            return result;
+            //return back the insertId for button to be created
+            return result.insertId;
         })
     },
     updateBurger : (id) => {
         orm.updateOne('burgers', 'devoured', 1, id, (result) => {
-            return result;
+            //return a bool which will determine if there was a successful update
+            //since if the number is 0, it will default to false, we can just return the number
+            return result.affectedRows;
         })
     }
 }
