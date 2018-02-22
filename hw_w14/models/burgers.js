7c83373d3e229b5685618dc279ad5cf90ecd7173
Kevin Castro <kcastro.build@gmail.com>
2018-02-22 10:59:51 -0800

    change returns to callback calls

--- hw_w14/models/burgers.js
+++ hw_w14/models/burgers.js
@@ -1,24 +1,25 @@
 const orm = require('../config/orm.js');
 
 const burgers = {
-    selectBurgers : () => {
+    selectBurgers : (callback) => {
         //get the callback to return the result array
         orm.selectAll('burgers', (result) => {
+            console.log(result)
             //do something with the result
-            return result;
+            callback(result);
         });
     },
-    insertBurger : (name) => {
+    insertBurger : (name, callback) => {
         orm.insertOne('burgers', 'burger_name', name, (result) => {
-            //return back the result
-            return result;
+            //do something with the result
+            callback(result);
         })
     },
-    updateBurger : (id) => {
+    updateBurger : (id, callback) => {
         orm.updateOne('burgers', 'devoured', 1, id, (result) => {
             //return a bool which will determine if there was a successful update
             //since if the number is 0, it will default to false, we can just return the number
-            return result.affectedRows;
+            callback(result.affectedRows);
         })
     }
 }
