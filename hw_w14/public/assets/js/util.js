154839e1688feed7b9b96a4018f82d879447a15d
Kevin Castro <kcastro.build@gmail.com>
2018-02-27 13:16:47 -0800

    fix submit onclick, add class put handler for each item

--- hw_w14/public/assets/js/util.js
+++ hw_w14/public/assets/js/util.js
@@ -1,23 +1,33 @@
 //on click for submit
-$('#submit').on('click', () => {
+$('#submit').on('click', (e) => {
+    e.preventDefault();
     //collect the data from the form
     const newBurger = {
         name : $('#new-burger').val().trim()
     }
 
-    console.log(newBurger)
-
     //create post request on db
     $.ajax('/burger', {
-        type : 'POST',
-        data : newBurger
-    }).then(() => {
-        location.reload();
-    });
+            type : 'POST',
+            data : newBurger
+        });
+
+    location.reload();
 });
 
 //onclick for the button in each non-devoured burger
+//no need for document listener since we reload every time a db change happens
+$('.not-devoured').on('click', function (e){
+    e.preventDefault();
 
-//on click for a list item, turns it into a form
+    //get the id of the burger
+    const id = $(this).data('id');
 
-//after getting information, create put request into db
+    //we actually just send the id to make the db change, we only need to send it once
+    $.ajax(`/burger/${id}`, {
+        type: 'PUT'
+    });
+
+    //refresh the page to reflect the change
+    location.reload();
+});
