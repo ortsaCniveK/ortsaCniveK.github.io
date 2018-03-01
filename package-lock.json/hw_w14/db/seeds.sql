29bb60bfdad8f01a3e67758d12982f35731d5d37
Kevin Castro <kcastro.build@gmail.com>
2018-03-01 08:26:26 -0800

    package-lock save state

---
+++ package-lock.json/hw_w14/db/seeds.sql
@@ -0,0 +1,17 @@
+-- Just to be safe
+use burgers_db;
+
+insert into burgers (burger_name)
+    values ('New Baconings');
+
+insert into burgers (burger_name)
+    values ('Olive and Let Die');
+
+insert into burgers (burger_name)
+    values ('Poblano Picasso');
+
+insert into burgers (burger_name)
+values ('If Looks Could Kale');
+
+insert into burgers (burger_name)
+values ('These Collards Don\'t Run');
