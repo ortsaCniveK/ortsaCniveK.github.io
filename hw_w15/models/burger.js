3e66e9c1d1899aef1444d516cd3d3e19a27568c0
Kevin Castro <kcastro.build@gmail.com>
2018-03-01 14:10:56 -0800

    add customer model, create validations

--- hw_w15/models/burger.js
+++ hw_w15/models/burger.js
@@ -1,11 +1,25 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var burger = sequelize.define('burger', {
-    burger_name: DataTypes.STRING,
-    devoured: DataTypes.BOOLEAN
+    burger_name: {
+        type : DataTypes.STRING,
+        validate : {
+            //doesn't allow empty string or null val
+            notNull : true,
+            notEmpty : true
+        }
+    }
+    devoured: {
+        type : DataTypes.BOOLEAN,
+        defaultValue : false
+    }
+    //foreign key
+    customer_id: DataTypes.INTEGER,
+
   }, {});
   burger.associate = function(models) {
-    // associations can be defined here
+      //set up association to customer
+      burger.belongsTo(models.customer, { foreign_key : customer_id });
   };
   return burger;
 };
---
+++ hw_w15/models/customer.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var customer = sequelize.define('customer', {
+    name: {
+        type : DataTypes.STRING,
+        validate : {
+            //doesn't allow empty string or null val
+            notNull : true,
+            notEmpty : true
+        }
+    }
+  }, {});
+  customer.associate = function(models) {
+    //establish association with burgers
+    customer.hasMany(models.burger, {onDelete : "cascade"});
+  };
+  return customer;
+};
